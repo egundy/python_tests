5cfbf60481d7b6ae98435ef192e44492bf798898
Evan <evangabe2002@gmail.com>
2022-01-05 19:03:36 -0600

    Create folder README.md for coding_bat folder. Make sleep_in.py.

---
+++ coding_bat/README.md
@@ -0,0 +1,1 @@
+Folder for puzzles from codingbat.com/python
---
+++ coding_bat/sleep_in.py
@@ -0,0 +1,17 @@
+'''Simple warmup to get started, no loops'''
+
+'''
+The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+
+
+sleep_in(False, False) → True
+sleep_in(True, False) → False
+sleep_in(False, True) → True'''
+
+def sleep_in(weekday , vacation):
+    if not weekday or vacation:
+        return True
+    else:
+        return False
+print("Shoud you sleep in?")
+print(sleep_in(False,False))
