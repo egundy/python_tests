6000a3776a3baa0bb02a60d0c540ffe36aacce44
Evan Gunderson <evangunderson21@gmail.com>
2022-04-18 10:41:19 -0500

    COLORS !!!!

--- RedlineCities/redlineSearch.py
+++ RedlineCities/redlineSearch.py
@@ -4,6 +4,19 @@
 # imports
 import pandas as pd
 
+# color class
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
 # dataframe
 df = pd.read_csv('redlinedCities.csv')
 
@@ -12,9 +25,9 @@ df = pd.read_csv('redlinedCities.csv')
 def state_search():
     n = 0
     while n == 0:
-        print("\nYou chose: state search")
-        state = input("What state do you want to search for? (Abbreviation only) type 'back' to return to category selector\n")
-        if state == ('exit'):
+        print(color.BLUE + "\nYou chose: state search" + color.END)
+        state = input(color.BLUE + "What state do you want to search for? (Abbreviation only) type 'back' to return to category selector\n" + color.END)
+        if state == ('back'):
             n = 1
         else:
             print(df[df["state"] == state])
@@ -22,8 +35,8 @@ def state_search():
 def city_search():
     n = 0
     while n == 0:
-        print("\nYou chose: city search")
-        city = input("What city do you want to search for? type 'back' to return to category selector\n")
+        print(color.YELLOW + "\nYou chose: city search" + color.END)
+        city = input(color.YELLOW + "What city do you want to search for? type 'back' to return to category selector\n" + color.END)
         if city == ('back'):
             n = 1
         else:
@@ -32,8 +45,8 @@ def city_search():
 def year_search():
     n = 0
     while n == 0:
-        print("\nYou chose: year search")
-        year = input("What year do you want to search for? type 'back' to return to category selector\n")
+        print(color.GREEN + "\nYou chose: year search" + color.END)
+        year = input(color.GREEN + "What year do you want to search for? type 'back' to return to category selector\n" + color.END)
         if year == ('back'):
             n = 1
         else:
@@ -55,12 +68,12 @@ def search_type(type):
         case "Year":
             year_search()
         case _: 
-            return "Unable to search for that category"
+            return color.BLUE + "Unable to search for that category" + color.END
   
 # main
 n = 0
 while n == 0:
-    type = input("What category do you want to search for?\n(Options: state, city, year) type 'exit' to exit\n\n")
+    type = input(color.DARKCYAN + "What category do you want to search for?\n(Options: state, city, year) type 'exit' to exit\n\n" + color.END)
     if type == ('exit'):
         n = 1
     search_type(type)
